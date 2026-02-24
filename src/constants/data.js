f84716ecf5eb7b9c5c65952f8c51512ce9fff208
Santiago Ocampo <sanntiocampo@gmail.com>
2026-02-24 10:58:30 -0300

    fix/python was added in techStack

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,6 +1,6 @@
 export const techStack = {
   frontend: ["React", "Next.js", "Vue.js", "TypeScript", "Three.js", "HTML / CSS"],
-  backend: ["Node.js", "Express", "Java", "Spring Boot", "PHP", "REST / GraphQL"],
+  backend: ["Node.js", "Express", "Java", "Spring Boot", "PHP", "Python", "REST / GraphQL"],
   data: ["PostgreSQL", "MySQL", "MongoDB", "Redis"],
   devops: ["AWS", "Docker", "Jenkins", "GitHub Actions", "CI/CD"],
 };
